10ed2c1cd2c032c23f16bff661b676cc61eec9a5
SnehansuBehera <snehansbehera080903@gmail.com>
2025-06-16 12:16:16 +0530

    Prisma-supabase-config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-
+import prisma from "./config/prisma-config.ts";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send("Hello, world");
 })
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany();
+    if (users.length === 0) {
+        res.status(404).json({ message: "No users found" });
+        return;
+    }
+    res.json(users);
+});
 
 app.listen(PORT, () => {
     console.log("Server is running at PORT:", PORT);
