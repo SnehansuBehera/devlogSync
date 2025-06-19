12d11bf618a8ba6b90b61079d7bbd228dac99722
SnehansuBehera <snehansbehera080903@gmail.com>
2025-06-19 10:17:08 +0530

    social-auth-added

--- src/config/prisma-config.ts
+++ src/config/prisma-config.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/index.js'
+import { PrismaClient } from '../generated/prisma/index'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as { 
