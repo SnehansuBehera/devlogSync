be757dc08fb4d29e00ffaef8728d66791d14f838
SnehansuBehera <snehansbehera080903@gmail.com>
2025-07-13 19:44:03 +0530

    lets deploy

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-  origin: ["http://localhost:3000"], 
+  origin: ["https://devlogsync.vercel.app","http://localhost:3000"], 
   credentials: true, //allow cookies
 }));
 
