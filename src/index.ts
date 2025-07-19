40fe89d985b82c56379186c8978ff6d2f04c5abc
SnehansuBehera <snehansbehera080903@gmail.com>
2025-07-19 17:59:55 +0530

    cors-updated

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,21 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
