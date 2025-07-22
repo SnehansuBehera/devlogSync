fff790b1b5c18236ec83b04ac81464f1318852ba
SnehansuBehera <snehansbehera080903@gmail.com>
2025-07-22 14:11:34 +0530

    cors-done

--- src/config/prisma-config.ts
+++ src/config/prisma-config.ts
@@ -1,21 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient
+const globalForPrisma = global as unknown as { 
+    prisma: PrismaClient
 }
 
-const client = new PrismaClient({
-  ...( {
-    __internal: {
-      engine: {
-        statementCacheSize: 0
-      }
-    }
-  } as any )
-}).$extends(withAccelerate())
-
-const prisma = globalForPrisma.prisma || client
+const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
